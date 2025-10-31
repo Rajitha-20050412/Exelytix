90063e98b4f7de9cd06c6e79aae45b2399d4f83c
KalangiGeetaneha <geetanehakalangi@gmail.com>
2025-10-31 22:21:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling effect for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    const targetId = this.getAttribute('href');
+    if (targetId.length > 1) {
+      e.preventDefault();
+      document.querySelector(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
